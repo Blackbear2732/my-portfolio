6694e2076505952b77da98cda382c84f4d7db88c
zhangjunyi <zhangjunyi2732@outlook.com>
2026-09-14 00:06:00 +0800

    增删了部分图片 修改了portfolio.js内的数据添加了符合目前情况的所有荣誉表现，修改了css内的分类情况。完善了个人成就的图片展示。

--- src/views/PortfolioView.vue
+++ src/views/PortfolioView.vue
@@ -11,12 +11,18 @@
     <div class="portfolio-grid">
       <div v-for="(item, i) in portfolioItems" :key="i" class="portfolio-item">
         <div class="portfolio-item-content">
+          <span class="portfolio-category">{{ item.category }}</span>
           <h3 v-html="item.title"></h3>
           <p class="portfolio-item-desc">{{ item.desc }}</p>
           <div class="portfolio-item-tech">
             <span v-for="tag in item.tags" :key="tag">{{ tag }}</span>
           </div>
-          <a href="#" class="portfolio-item-link" @click.prevent="openCert(item.certificate)">查看证明</a>
+          <a
+              v-if="item.certificate"
+              href="#"
+              class="portfolio-item-link"
+              @click.prevent="openCert(item.certificate)"
+          >查看证明</a>
         </div>
       </div>
     </div>
@@ -25,15 +31,15 @@
   <Teleport to="body">
     <div v-if="certVisible" class="certificate-viewer" style="display:flex" @click.self="certVisible = false">
       <span class="close-certificate" @click="certVisible = false">&times;</span>
-      <img :src="asset(certSrc)" alt="证明" />
+      <img :src="asset(certSrc)" alt="证明"/>
     </div>
   </Teleport>
 </template>
 
 <script setup>
-import { ref } from 'vue'
-import { portfolioItems } from '@/data/portfolio'
-import { asset } from '@/utils/asset'
+import {ref} from 'vue'
+import {portfolioItems} from '@/data/portfolio'
+import {asset} from '@/utils/asset'
 
 const certVisible = ref(false)
 const certSrc = ref('')
